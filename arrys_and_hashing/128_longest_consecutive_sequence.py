d2a672d0f7d44ab47908253779525de190a5a10b
James Shoemake <james.shoemake1@gmail.com>
2023-05-24 08:56:14 -0500

    added 128 longest consecutive sequence

---
+++ arrys_and_hashing/128_longest_consecutive_sequence.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        numSet = set(nums)
+        longest = 0
+
+        for n in numSet:
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
