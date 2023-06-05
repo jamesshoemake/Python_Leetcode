9a4594684ce8efba4603850eb69b6bcbdc89492f
James Shoemake <james.shoemake1@gmail.com>
2023-06-05 09:37:15 -0500

    added 84 largest rectangle in histogram

---
+++ stack/84_largest_rect_in_histogram.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        maxArea = 0
+        stack = []  # two elements (index, height)
+
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                # determine if current area great than maxArea
+                # height * (i - index) gets current area
+                maxArea = max(maxArea, height * (i - index))
+                start = index
+            stack.append((start, height))
+
+        for i, h in stack:
+            maxArea = max(maxArea, h * (len(heights) - i))
+        return maxArea
