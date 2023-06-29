66a6b73e75c1cefc752d9ceef1b45684d6428154
James Shoemake <james.shoemake1@gmail.com>
2023-06-29 10:22:40 -0500

    added 239 sliding window max

---
+++ sliding_window/239_sliding_window_max.py
@@ -0,0 +1,23 @@
+from typing import List
+import collections
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        que = collections.deque()  # que of indexes
+        l = r = 0
+
+        while r < len(nums):
+            while que and nums[que[-1]] < nums[r]:
+                que.pop()
+            que.append(r)
+
+            if l > que[0]:
+                que.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[que[0]])
+                l += 1
+            r += 1
+        return output
