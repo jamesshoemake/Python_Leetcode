9c16f347e5f6c46ff047de5cc36e2f60f4cc5b06
James Shoemake <james.shoemake1@gmail.com>
2023-07-03 09:24:56 -0500

    added 2 solution to 206 reverse linked list

---
+++ linked_list/206_reverse_linked_list.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# best solution Time O(n), Memory O(1)
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+
+# Time O(n), Memory O(n)
+
+
+class RecursiveSolution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
