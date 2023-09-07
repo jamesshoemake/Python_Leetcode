262ea5590a1f060b2af21b3f580fee07df4eac58
James Shoemake <james.shoemake1@gmail.com>
2023-09-07 08:15:40 -0500

    134 gas station

---
+++ greedy/134_gas_station.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        total_gas = 0
+        remaining_gas = 0
+        start_index = 0
+
+        for i in range(len(gas)):
+            total_gas += gas[i] - cost[i]
+            remaining_gas += gas[i] - cost[i]
+
+            if remaining_gas < 0:
+                remaining_gas = 0
+                start_index = i + 1
+
+        if total_gas >= 0:
+            return start_index
+        else:
+            return -1
